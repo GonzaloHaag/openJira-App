0db6fbd2c2c8e5e3aef5e85c5bd4c6df39fc133a
GonzaloHaag <gonzalohaag13@gmail.com>
2023-12-08 12:21:32 -0300

    Volvi a la normalidad el baseUrl

--- apis/entradasApi.ts
+++ apis/entradasApi.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 
 const entradasApi = axios.create({
     //Como sale del mismo servidor, solo pongo lo que quiero como base, esto lo interpeta como localhost:3000/api y luego para llamarlo pongo lo que le sigue al endpoint de api    
-    baseURL:'https://vercel.com/gonzalohaag/open-jira-app/api'
+    baseURL:'/api'
 });
 export default entradasApi;
